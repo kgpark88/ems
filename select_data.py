029e4c4d7cc2227faba0bd149f80807aea274fbe
Danny Park <kgpark88@gmail.com>
2019-04-21 05:48:26 +0900

    energy data select program

--- select_data.py
+++ select_data.py
@@ -2,15 +2,15 @@ import sqlite3
 
 con = sqlite3.connect("bems.db")
 
-with con:     
-    cursor = con.cursor()   
-    # 2016년 1월 10일 10일의 10시대의 데이터만 조회
+with con:
+    cursor = con.cursor()
+    # 2019년 01월 07일 15시 데이터만 조회
     cursor.execute(" SELECT date_time, usage, peak FROM energy \
-                     WHERE b_code ='SGBD' AND date_time LIKE '2016011010%' ")
+                     WHERE b_code ='SGBD' AND date_time LIKE '2019010715%' ")
 
     rows = cursor.fetchall()
 
     for row in rows:
-        print(row)        
-        
+        print(row)
+
 con.close()
