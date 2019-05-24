aaf43e9d1b45f4aaff8b3f180ab32a2761a7af36
Danny Park <kgpark88@gmail.com>
2019-05-25 04:47:48 +0900

    weather.py

--- weather.py
+++ weather.py
@@ -1,19 +1,18 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-
-japi = "http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=4111760000" 
+# 동네예보 : 서울특별시 송파구 방이1동
+data_name = "서울특별시 송파구 방이1동"
+data_code = "1171056100"
+japi = "http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=" + data_code
 response = urlopen(japi)
 weather = BeautifulSoup(response, "html.parser")
-    
-    
+
+print(data_name, " 동네 예보")
+print("데이터 소스 :", japi)
+
 for data in weather.findAll('data'):
     hour = data.hour.string
     temp = data.temp.string
-    reh  = data.reh.string
-    print ("Hour : %2s, Temperature : %04s, Humidity : %02s "  % ( hour, temp, reh ))
-    
-
-
-
- 
+    reh = data.reh.string
+    print("시간 : %02s, 온도 : %04s, 습도 : %02s " % (hour, temp, reh ))
